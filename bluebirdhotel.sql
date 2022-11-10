a1f7cd859abc08998c73a8660463afa88bb3f0f8
Tushar Pankhaniya <87109400+tusharpankhaniya@users.noreply.github.com>
2022-11-10 23:48:05 +0530

    Add files via upload

---
+++ bluebirdhotel.sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2022 at 11:19 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bluebirdhotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp_login`
+--
+
+CREATE TABLE `emp_login` (
+  `empid` int(100) NOT NULL,
+  `Emp_Email` varchar(50) NOT NULL,
+  `Emp_Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `emp_login`
+--
+
+INSERT INTO `emp_login` (`empid`, `Emp_Email`, `Emp_Password`) VALUES
+(1, 'Admin@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `NoofRoom` int(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `noofdays` int(30) NOT NULL,
+  `roomtotal` double(8,2) NOT NULL,
+  `bedtotal` double(8,2) NOT NULL,
+  `meal` varchar(30) NOT NULL,
+  `mealtotal` double(8,2) NOT NULL,
+  `finaltotal` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `Name`, `Email`, `RoomType`, `Bed`, `NoofRoom`, `cin`, `cout`, `noofdays`, `roomtotal`, `bedtotal`, `meal`, `mealtotal`, `finaltotal`) VALUES
+(41, 'Tushar pankhaniya', 'pankhaniyatushar9@gmail.com', 'Single Room', 'Single', 1, '2022-11-09', '2022-11-10', 1, 1000.00, 10.00, 'Room only', 0.00, 1010.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(30) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `bedding` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `type`, `bedding`) VALUES
+(4, 'Superior Room', 'Single'),
+(6, 'Superior Room', 'Triple'),
+(7, 'Superior Room', 'Quad'),
+(8, 'Deluxe Room', 'Single'),
+(9, 'Deluxe Room', 'Double'),
+(10, 'Deluxe Room', 'Triple'),
+(11, 'Guest House', 'Single'),
+(12, 'Guest House', 'Double'),
+(13, 'Guest House', 'Triple'),
+(14, 'Guest House', 'Quad'),
+(16, 'Superior Room', 'Double'),
+(20, 'Single Room', 'Single'),
+(22, 'Superior Room', 'Single'),
+(23, 'Deluxe Room', 'Single'),
+(24, 'Deluxe Room', 'Triple'),
+(27, 'Guest House', 'Double'),
+(30, 'Deluxe Room', 'Single');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roombook`
+--
+
+CREATE TABLE `roombook` (
+  `id` int(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Country` varchar(30) NOT NULL,
+  `Phone` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `Meal` varchar(30) NOT NULL,
+  `NoofRoom` varchar(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `nodays` int(50) NOT NULL,
+  `stat` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `roombook`
+--
+
+INSERT INTO `roombook` (`id`, `Name`, `Email`, `Country`, `Phone`, `RoomType`, `Bed`, `Meal`, `NoofRoom`, `cin`, `cout`, `nodays`, `stat`) VALUES
+(41, 'Tushar pankhaniya', 'pankhaniyatushar9@gmail.com', 'India', '9313346569', 'Single Room', 'Single', 'Room only', '1', '2022-11-09', '2022-11-10', 1, 'Confirm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `UserID` int(100) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `signup`
+--
+
+INSERT INTO `signup` (`UserID`, `Username`, `Email`, `Password`) VALUES
+(1, 'Tushar Pankhaniya', 'tusharpankhaniya2202@gmail.com', '123'),
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `work` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `name`, `work`) VALUES
+(1, 'Tushar pankhaniya', 'Manager'),
+(3, 'rohit patel', 'Cook'),
+(4, 'Dipak', 'Cook'),
+(5, 'tirth', 'Helper'),
+(6, 'mohan', 'Helper'),
+(7, 'shyam', 'cleaner'),
+(8, 'rohan', 'weighter'),
+(9, 'hiren', 'weighter'),
+(10, 'nikunj', 'weighter'),
+(11, 'rekha', 'Cook');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emp_login`
+--
+ALTER TABLE `emp_login`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roombook`
+--
+ALTER TABLE `roombook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emp_login`
+--
+ALTER TABLE `emp_login`
+  MODIFY `empid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `roombook`
+--
+ALTER TABLE `roombook`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `UserID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
